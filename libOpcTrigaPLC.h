058258a33709f65391650f6966c62c769467ec80
Thalles Campagnani <thallescampagnani@gmail.com>
2024-03-14 17:30:51 -0300

    Estrutura básica

---
+++ libOpcTrigaPLC.h
@@ -0,0 +1,81 @@
+/*
+libOpcTrigaPLC is a library to communicate with the Triga PLC
+using OPC UA client (Ethernet) on a GNU operating system.
+Copyright (C) 2024 Thalles Campagnani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIB_OPC_TRIGA_PLC
+#define LIB_OPC_TRIGA_PLC
+
+#include <stdexcept>
+#include <iostream>
+
+struct PLC_DATA
+{
+    int   STATE     = 0;
+    //
+    float BarraReg      = 0;//ns=2;s=GVL.BarraReg   //Barra de Regulação
+    float BarraCon      = 0;//ns=2;s=GVL.BarraCon   //Barra de Controle
+    float BarraSeg      = 0;//ns=2;s=GVL.BarraSeg   //Barra de Segurança
+    //
+    float CLogALog      = 0;//ns=2;s=GVL.CLogALog   //Canal Logaritico - Aquisição Logarítima
+    float CLogALin      = 0;//ns=2;s=GVL.CLogALin   //Canal Logaritico - Aquisição Linear
+    float CLogAPer      = 0;//ns=2;s=GVL.CLogAPer   //Canal Logarítimo - Aquisição Período
+    //
+    /*
+    float CParALin      = 0;//ns=2;s=GVL.CParALin   //Canal Partida - Aquisição Linear
+    float CParALog      = 0;//ns=2;s=GVL.CParALog   //Canal Partida - Aquisição Log
+    float CParAPer      = 0;//ns=2;s=GVL.CParAPer   //Canal Partida - Aquisição Período
+    //
+    float CLogARea      = 0;//ns=2;s=GVL.CLogARea   //Canal Logarítimo - Aquisição Reativímetro
+    float CLin          = 0;//ns=2;s=GVL.CLin       //Canal Linear
+    float CPer          = 0;//ns=2;s=GVL.CPer       //Canal Percentual
+    //
+    float SRadAre       = 0;//ns=2;s=GVL.SRadAre    //Sensor Radiação Área
+    float SRadEntPri    = 0;//ns=2;s=GVL.SRadEntPri //Sensor Radiação Entrada Primário
+    float SRadPoc       = 0;//ns=2;s=GVL.SRadPoc    //Sensor Radiação Poço
+    float SRadRes       = 0;//ns=2;s=GVL.SRadRes    //Sensor Radiação Resinas
+    float SRadSaiSec    = 0;//ns=2;s=GVL.SRadSaiSec //Sensor Radiação Saída Secundário
+    float SRadAer       = 0;//ns=2;s=GVL.SRadAer    //Sensor Radiação Aerossois
+    //
+    float VasPri        = 0;//ns=2;s=GVL.VasPri     //Sensor Vazão Sistema Primário de Refrigeração
+    //
+    float SPt100Poco    = 0;//ns=2;s=GVL.SPt100Poco     //PT100_TEMP_POCO
+    float SPt100EntPri  = 0;//ns=2;s=GVL.SPt100EntPri   //PT100_TEMP_ENT_PRIM
+    float SPt100SaiPri  = 0;//ns=2;s=GVL.SPt100SaiPri   //PT100_TEMP_SAI_PRIM
+    float SPt100EntSec  = 0;//ns=2;s=GVL.SPt100EntSec   //PT100_TEMP_ENT_SEC
+    float SPt100SaiSec  = 0;//ns=2;s=GVL.SPt100SaiSec   //PT100_TEMP_SAI_SEC
+    float STpPoc1       = 0;//ns=2;s=GVL.STpPoc1        //TERMOPAR_1_POCO
+    float STpPoc2       = 0;//ns=2;s=GVL.STpPoc2        //TERMOPAR_2_POCO
+    float STpLen        = 0;//ns=2;s=GVL.STpLen         //TERMOPAR_3_LENCOL
+    float SConPoc       = 0;//ns=2;s=GVL.SConPoc        //COND_POCO
+    float SConSaiPri    = 0;//ns=2;s=GVL.SConSaiPri     //COND_SAI_PRI
+    */
+};
+
+
+class libOpcTrigaPLC {
+public:
+    libOpcTrigaPLC(std::string ipAddress, int port);
+    ~libOpcTrigaPLC();
+
+    PLC_DATA get_all();
+
+private:
+    PLC_DATA plcData;
+};
+
+#endif // LIB_OPC_TRIGA_PLC
---
+++ test.cpp
@@ -0,0 +1,69 @@
+/*
+This is a test program for libOpcTrigaPLC, a library to communicate with 
+the Triga PLC using OPC UA client (Ethernet) on a GNU operating system.
+Copyright (C) 2024 Thalles Campagnani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "libOpcTrigaPLC.h"
+#include <string>
+#include <chrono>
+
+std::string getTimeString()
+{
+    static std::chrono::time_point<std::chrono::high_resolution_clock> tempoInicial = std::chrono::high_resolution_clock::now();
+
+    // Obtém o tempo atual
+    std::time_t tempoAtualRaw = std::time(nullptr);
+    struct tm* tempoInfo = std::localtime(&tempoAtualRaw);
+
+    // Obtém horas, minutos, segundos e milissegundos
+    int horas = tempoInfo->tm_hour;
+    int minutos = tempoInfo->tm_min;
+    int segundos = tempoInfo->tm_sec;
+    std::string millisegundos = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() % 1000);
+
+    // Calcula diferença de tempo em microssegundos
+    auto tempoAtual = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+    static auto tempoAnterior = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+    auto diferencaMicros = tempoAtual - tempoAnterior;
+    tempoAnterior = tempoAtual;
+
+    // Formata a string
+    std::string horaFormatada = std::to_string(horas) + ":" + std::to_string(minutos) + ":" + std::to_string(segundos) + "." + millisegundos + "\t [" + std::to_string(diferencaMicros.count()) + "]";
+
+    return horaFormatada;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <ip> <port> <qtd_of_reads>" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        libOpcTrigaPLC plc(argv[1], std::stoi(argv[2]));
+        for(int i=0;i<std::stoi(argv[3]);i++)
+        {
+            PLC_DATA data = plc.get_all();
+            std::cout << getTimeString() << "\t" << data.CLogALog << std::endl;
+        }
+    }
+    catch(int error) { return 2; }
+    return 0;
+}

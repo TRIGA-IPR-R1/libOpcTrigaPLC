72913d5150112c706bb4b8949754ea78f27e2b42
Álan Crístoffer <acristoffers@startmail.com>
2024-03-26 11:52:06 -0300

    fix: use opaque pointer
    This avoids including a dependency's header in our header.

--- include/libOpcTrigaPLC.h
+++ include/libOpcTrigaPLC.h
@@ -17,69 +17,89 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#ifndef LIB_OPC_TRIGA_PLC
-#define LIB_OPC_TRIGA_PLC
+#pragma once
 
-#include <stdexcept>
 #include <iostream>
-#include <open62541pp/open62541pp.h>
+#include <stdexcept>
 
-struct PLC_DATA
-{
-    int   STATE         = -1;   //Status do PLC:
-                                //-1 = Nenhuma tentativa de conexão realizada ainda.
-                                // 0 = Valores lidos com sucesso.
-                                // 1 = Erro de leitura
-                                // 2 = Servidor desconectado
-
-    //
-    float BarraReg      = -1;//ns=2;IoConfig_Globals_Mapping.inBarraReg (%IW5)      //Barra de Regulação
-    float BarraCon      = -1;//ns=2;IoConfig_Globals_Mapping.inBarraCon (%IW6)      //Barra de Controle
-    float BarraSeg      = -1;//ns=2;IoConfig_Globals_Mapping.inBarraSeg (%IW7)      //Barra de Segurança
-    float CLogALog      = -1;//ns=2;IoConfig_Globals_Mapping.inCLogALog (%IW17)     //Canal Logaritico - Aquisição Logarítima
-    float CLogALin      = -1;//ns=2;IoConfig_Globals_Mapping.inCLogALin (%IW18)     //Canal Logaritico - Aquisição Linear
-    float CLogAPer      = -1;//ns=2;IoConfig_Globals_Mapping.inCLogAPer (%IW19)     //Canal Logarítimo - Aquisição Período
-    float CParALin      = -1;//ns=2;IoConfig_Globals_Mapping.inCParALin (%IW14)     //Canal Partida - Aquisição Linear
-    float CParALog      = -1;//ns=2;IoConfig_Globals_Mapping.inCParALog (%IW15)     //Canal Partida - Aquisição Log
-    float CParAPer      = -1;//ns=2;IoConfig_Globals_Mapping.inCParAPer (%IW16)     //Canal Partida - Aquisição Período
-    float CLogARea      = -1;//ns=2;IoConfig_Globals_Mapping.inCLogARea (%IW20)     //Canal Logarítimo - Aquisição Reativímetro
-    float CLin          = -1;//ns=2;IoConfig_Globals_Mapping.inCLin (%IW25)         //Canal Linear
-    int   CLinScale     = -1;//ns=2;IoConfig_Globals_Mapping.inModulo_D0 (%IW0)     //Canal Linear - Escala (Bit 0, 1 e 3)
-    float CPer          = -1;//ns=2;IoConfig_Globals_Mapping.inCPer (%IW26)         //Canal Percentual
-    float SRadAre       = -1;//ns=2;IoConfig_Globals_Mapping.inSRadAre (%IW27)      //Sensor Radiação Área
-    float SRadEntPri    = -1;//ns=2;IoConfig_Globals_Mapping.inSRadEntPri (%IW28)   //Sensor Radiação Entrada Primário
-    float SRadPoc       = -1;//ns=2;IoConfig_Globals_Mapping.inSRadPoc (%IW29)      //Sensor Radiação Poço
-    float SRadRes       = -1;//ns=2;IoConfig_Globals_Mapping.inSRadRes (%IW30)      //Sensor Radiação Resinas
-    float SRadSaiSec    = -1;//ns=2;IoConfig_Globals_Mapping.inSRadSaiSec (%IW31)   //Sensor Radiação Saída Secundário
-    float SRadAer       = -1;//ns=2;IoConfig_Globals_Mapping.inSRadAer (%IW32)      //Sensor Radiação Aerossois
-    float SVasPri       = -1;//ns=2;IoConfig_Globals_Mapping.inSVasPri (%IW49)      //Sensor Vazão Sistema Primário de Refrigeração
-    float SPt100Poco    = -1;//ns=2;s=GVL.SPt100Poco (%MD1)                         //PT100_TEMP_POCO
-    float SPt100EntPri  = -1;//ns=2;s=GVL.SPt100EntPri (%MD2)                       //PT100_TEMP_ENT_PRIM
-    float SPt100SaiPri  = -1;//ns=2;s=GVL.SPt100SaiPri (%MD3)                       //PT100_TEMP_SAI_PRIM
-    float SPt100EntSec  = -1;//ns=2;s=GVL.SPt100EntSec (%MD4)                       //PT100_TEMP_ENT_SEC
-    float SPt100SaiSec  = -1;//ns=2;s=GVL.SPt100SaiSec (%MD5)                       //PT100_TEMP_SAI_SEC
-    float STpPoc1       = -1;//ns=2;s=GVL.STpPoc1 (%MD6)                            //TERMOPAR_1_POCO
-    float STpPoc2       = -1;//ns=2;s=GVL.STpPoc2 (%MD7)                            //TERMOPAR_2_POCO
-    float STpLen        = -1;//ns=2;s=GVL.STpLen (%MD8)                             //TERMOPAR_3_LENCOL
-    float SConPoc       = -1;//ns=2;s=GVL.SConPoc (%MD9)                            //COND_POCO
-    float SConSaiPri    = -1;//ns=2;s=GVL.SConSaiPri (%MD10)                        //COND_SAI_PRI
+struct PLC_DATA {
+  int STATE = -1; // Status do PLC:
+                  //-1 = Nenhuma tentativa de conexão realizada ainda.
+                  //  0 = Valores lidos com sucesso.
+                  //  1 = Erro de leitura
+                  //  2 = Servidor desconectado
+  //
+  float BarraReg = -1; // ns=2;IoConfig_Globals_Mapping.inBarraReg (%IW5)
+                       // //Barra de Regulação
+  float BarraCon = -1; // ns=2;IoConfig_Globals_Mapping.inBarraCon (%IW6)
+                       // //Barra de Controle
+  float BarraSeg = -1; // ns=2;IoConfig_Globals_Mapping.inBarraSeg (%IW7)
+                       // //Barra de Segurança
+  float CLogALog = -1; // ns=2;IoConfig_Globals_Mapping.inCLogALog (%IW17)
+                       // //Canal Logaritico - Aquisição Logarítima
+  float CLogALin = -1; // ns=2;IoConfig_Globals_Mapping.inCLogALin (%IW18)
+                       // //Canal Logaritico - Aquisição Linear
+  float CLogAPer = -1; // ns=2;IoConfig_Globals_Mapping.inCLogAPer (%IW19)
+                       // //Canal Logarítimo - Aquisição Período
+  float CParALin = -1; // ns=2;IoConfig_Globals_Mapping.inCParALin (%IW14)
+                       // //Canal Partida - Aquisição Linear
+  float CParALog = -1; // ns=2;IoConfig_Globals_Mapping.inCParALog (%IW15)
+                       // //Canal Partida - Aquisição Log
+  float CParAPer = -1; // ns=2;IoConfig_Globals_Mapping.inCParAPer (%IW16)
+                       // //Canal Partida - Aquisição Período
+  float CLogARea = -1; // ns=2;IoConfig_Globals_Mapping.inCLogARea (%IW20)
+                       // //Canal Logarítimo - Aquisição Reativímetro
+  float CLin =
+      -1; // ns=2;IoConfig_Globals_Mapping.inCLin (%IW25)         //Canal Linear
+  int CLinScale = -1; // ns=2;IoConfig_Globals_Mapping.inModulo_D0 (%IW0)
+                      // //Canal Linear - Escala (Bit 0, 1 e 3)
+  float CPer = -1; // ns=2;IoConfig_Globals_Mapping.inCPer (%IW26) //Canal
+                   // Percentual
+  float SRadAre = -1; // ns=2;IoConfig_Globals_Mapping.inSRadAre (%IW27)
+                      // //Sensor Radiação Área
+  float SRadEntPri = -1; // ns=2;IoConfig_Globals_Mapping.inSRadEntPri (%IW28)
+                         // //Sensor Radiação Entrada Primário
+  float SRadPoc = -1; // ns=2;IoConfig_Globals_Mapping.inSRadPoc (%IW29)
+                      // //Sensor Radiação Poço
+  float SRadRes = -1; // ns=2;IoConfig_Globals_Mapping.inSRadRes (%IW30)
+                      // //Sensor Radiação Resinas
+  float SRadSaiSec = -1; // ns=2;IoConfig_Globals_Mapping.inSRadSaiSec (%IW31)
+                         // //Sensor Radiação Saída Secundário
+  float SRadAer = -1; // ns=2;IoConfig_Globals_Mapping.inSRadAer (%IW32)
+                      // //Sensor Radiação Aerossois
+  float SVasPri = -1; // ns=2;IoConfig_Globals_Mapping.inSVasPri (%IW49)
+                      // //Sensor Vazão Sistema Primário de Refrigeração
+  float SPt100Poco = -1; // ns=2;s=GVL.SPt100Poco (%MD1) //PT100_TEMP_POCO
+  float SPt100EntPri =
+      -1; // ns=2;s=GVL.SPt100EntPri (%MD2) //PT100_TEMP_ENT_PRIM
+  float SPt100SaiPri =
+      -1; // ns=2;s=GVL.SPt100SaiPri (%MD3) //PT100_TEMP_SAI_PRIM
+  float SPt100EntSec =
+      -1; // ns=2;s=GVL.SPt100EntSec (%MD4) //PT100_TEMP_ENT_SEC
+  float SPt100SaiSec =
+      -1;                // ns=2;s=GVL.SPt100SaiSec (%MD5) //PT100_TEMP_SAI_SEC
+  float STpPoc1 = -1;    // ns=2;s=GVL.STpPoc1 (%MD6) //TERMOPAR_1_POCO
+  float STpPoc2 = -1;    // ns=2;s=GVL.STpPoc2 (%MD7) //TERMOPAR_2_POCO
+  float STpLen = -1;     // ns=2;s=GVL.STpLen (%MD8) //TERMOPAR_3_LENCOL
+  float SConPoc = -1;    // ns=2;s=GVL.SConPoc (%MD9) //COND_POCO
+  float SConSaiPri = -1; // ns=2;s=GVL.SConSaiPri (%MD10) //COND_SAI_PRI
 };
 
 void libOpcTrigaPLC_license();
 
+struct libOpcTrigaPLC_private;
+
 class libOpcTrigaPLC {
 public:
-    libOpcTrigaPLC(std::string address);
-    ~libOpcTrigaPLC();
+  libOpcTrigaPLC(std::string address);
+  ~libOpcTrigaPLC();
 
-    PLC_DATA get_all();
-    bool tryConnect();
+  PLC_DATA get_all();
+  bool tryConnect();
 
 private:
-    opcua::Client client;
-    PLC_DATA plcData;
-    std::string serverAddress;
-    std::string stdErrorMsg(std::string functionName, std::string errorMsg, std::string exptionMsg);
-};
+  libOpcTrigaPLC_private *_p;
 
-#endif // LIB_OPC_TRIGA_PLC
+  std::string stdErrorMsg(std::string functionName, std::string errorMsg,
+                          std::string exptionMsg);
+};
--- src/libOpcTrigaPLC.cpp
+++ src/libOpcTrigaPLC.cpp
@@ -19,6 +19,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <libOpcTrigaPLC.h>
 
+#include <open62541pp/open62541pp.h>
+
+struct libOpcTrigaPLC_private {
+    opcua::Client client;
+    PLC_DATA plcData;
+    std::string serverAddress;
+};
+
 void libOpcTrigaPLC_license()
 {
     std::cout << "libOpcTrigaPLC    Copyright (C) 2024 Thalles Campagnani" << std::endl;
@@ -32,7 +40,7 @@ bool libOpcTrigaPLC::tryConnect()
 {
     try
     {
-        client.connect(serverAddress);
+        this->_p->client.connect(this->_p->serverAddress);
     }
     catch (const std::exception& e)
     {
@@ -59,65 +67,67 @@ std::string libOpcTrigaPLC::stdErrorMsg(std::string functionName, std::string er
 
 libOpcTrigaPLC::libOpcTrigaPLC(std::string address)
 {
-    serverAddress = "opc.tcp://" + address;
+    this->_p = new libOpcTrigaPLC_private;
+    this->_p->serverAddress = "opc.tcp://" + address;
     tryConnect();
 }
 
 libOpcTrigaPLC::~libOpcTrigaPLC()
 {
-    client.disconnect();
+    this->_p->client.disconnect();
+    delete this->_p;
 }
 
 PLC_DATA libOpcTrigaPLC::get_all()
 {
     try
     {
-        plcData.BarraReg        = client.getNode({2, "IoConfig_Globals_Mapping.inBarraReg (%IW5)"})   .readValueScalar<int16_t>();
-        plcData.BarraCon        = client.getNode({2, "IoConfig_Globals_Mapping.inBarraCon (%IW6)"})   .readValueScalar<int16_t>();
-        plcData.BarraSeg        = client.getNode({2, "IoConfig_Globals_Mapping.inBarraSeg (%IW7)"})   .readValueScalar<int16_t>();
-        plcData.CLogALog        = client.getNode({2, "IoConfig_Globals_Mapping.inCLogALog (%IW17)"})  .readValueScalar<int16_t>();
-        plcData.CLogALin        = client.getNode({2, "IoConfig_Globals_Mapping.inCLogALin (%IW18)"})  .readValueScalar<int16_t>();
-        plcData.CLogAPer        = client.getNode({2, "IoConfig_Globals_Mapping.inCLogAPer (%IW19)"})  .readValueScalar<int16_t>();
-        plcData.CParALin        = client.getNode({2, "IoConfig_Globals_Mapping.inCParALin (%IW14)"})  .readValueScalar<int16_t>();
-        plcData.CParALog        = client.getNode({2, "IoConfig_Globals_Mapping.inCParALog (%IW15)"})  .readValueScalar<int16_t>();
-        plcData.CParAPer        = client.getNode({2, "IoConfig_Globals_Mapping.inCParAPer (%IW16)"})  .readValueScalar<int16_t>();
-        plcData.CLogARea        = client.getNode({2, "IoConfig_Globals_Mapping.inCLogARea (%IW20)"})  .readValueScalar<int16_t>();
-        plcData.CLin            = client.getNode({2, "IoConfig_Globals_Mapping.inCLin (%IW25)"})      .readValueScalar<int16_t>();
-        plcData.CLinScale       = client.getNode({2, "IoConfig_Globals_Mapping.inModule_D0 (%IW0)"})  .readValueScalar<u_int16_t>() & 0b00000111 - 2;
-        plcData.CPer            = client.getNode({2, "IoConfig_Globals_Mapping.inCPer (%IW26)"})      .readValueScalar<int16_t>();
-        plcData.SRadAre         = client.getNode({2, "IoConfig_Globals_Mapping.inSRadAre (%IW27)"})   .readValueScalar<int16_t>();
-        plcData.SRadEntPri      = client.getNode({2, "IoConfig_Globals_Mapping.inSRadEntPri (%IW28)"}).readValueScalar<int16_t>();
-        plcData.SRadPoc         = client.getNode({2, "IoConfig_Globals_Mapping.inSRadPoc (%IW29)"})   .readValueScalar<int16_t>();
-        plcData.SRadRes         = client.getNode({2, "IoConfig_Globals_Mapping.inSRadRes (%IW30)"})   .readValueScalar<int16_t>();
-        plcData.SRadSaiSec      = client.getNode({2, "IoConfig_Globals_Mapping.inSRadSaiSec (%IW31)"}).readValueScalar<int16_t>();
-        plcData.SRadAer         = client.getNode({2, "IoConfig_Globals_Mapping.inSRadAer (%IW32)"})   .readValueScalar<int16_t>();
-        plcData.SVasPri         = client.getNode({2, "IoConfig_Globals_Mapping.inSVasPri (%IW49)"})   .readValueScalar<int16_t>();
-        plcData.SPt100Poco      = client.getNode({2, "GVL.SPt100Poco (%MD1)"})                        .readValueScalar<float>();
-        plcData.SPt100EntPri    = client.getNode({2, "GVL.SPt100EntPri (%MD2)"})                      .readValueScalar<float>();
-        plcData.SPt100SaiPri    = client.getNode({2, "GVL.SPt100SaiPri (%MD3)"})                      .readValueScalar<float>();
-        plcData.SPt100EntSec    = client.getNode({2, "GVL.SPt100EntSec (%MD4)"})                      .readValueScalar<float>();
-        plcData.SPt100SaiSec    = client.getNode({2, "GVL.SPt100SaiSec (%MD5)"})                      .readValueScalar<float>();
-        plcData.STpPoc1         = client.getNode({2, "GVL.STpPoc1 (%MD6)"})                           .readValueScalar<float>();
-        plcData.STpPoc2         = client.getNode({2, "GVL.STpPoc2 (%MD7)"})                           .readValueScalar<float>();
-        plcData.STpLen          = client.getNode({2, "GVL.STpLen (%MD8)"})                            .readValueScalar<float>();
-        plcData.SConPoc         = client.getNode({2, "GVL.SConPoc (%MD9)"})                           .readValueScalar<float>();
-        plcData.SConSaiPri      = client.getNode({2, "GVL.SConSaiPri (%MD10)"})                       .readValueScalar<float>();
-        plcData.STATE = 0;
+        this->_p->plcData.BarraReg        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inBarraReg (%IW5)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.BarraCon        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inBarraCon (%IW6)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.BarraSeg        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inBarraSeg (%IW7)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.CLogALog        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCLogALog (%IW17)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CLogALin        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCLogALin (%IW18)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CLogAPer        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCLogAPer (%IW19)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CParALin        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCParALin (%IW14)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CParALog        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCParALog (%IW15)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CParAPer        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCParAPer (%IW16)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CLogARea        = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCLogARea (%IW20)"})  .readValueScalar<int16_t>();
+        this->_p->plcData.CLin            = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCLin (%IW25)"})      .readValueScalar<int16_t>();
+        this->_p->plcData.CLinScale       = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inModule_D0 (%IW0)"})  .readValueScalar<u_int16_t>() & 0b00000111 - 2;
+        this->_p->plcData.CPer            = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inCPer (%IW26)"})      .readValueScalar<int16_t>();
+        this->_p->plcData.SRadAre         = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSRadAre (%IW27)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.SRadEntPri      = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSRadEntPri (%IW28)"}).readValueScalar<int16_t>();
+        this->_p->plcData.SRadPoc         = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSRadPoc (%IW29)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.SRadRes         = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSRadRes (%IW30)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.SRadSaiSec      = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSRadSaiSec (%IW31)"}).readValueScalar<int16_t>();
+        this->_p->plcData.SRadAer         = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSRadAer (%IW32)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.SVasPri         = this->_p->client.getNode({2, "IoConfig_Globals_Mapping.inSVasPri (%IW49)"})   .readValueScalar<int16_t>();
+        this->_p->plcData.SPt100Poco      = this->_p->client.getNode({2, "GVL.SPt100Poco (%MD1)"})                        .readValueScalar<float>();
+        this->_p->plcData.SPt100EntPri    = this->_p->client.getNode({2, "GVL.SPt100EntPri (%MD2)"})                      .readValueScalar<float>();
+        this->_p->plcData.SPt100SaiPri    = this->_p->client.getNode({2, "GVL.SPt100SaiPri (%MD3)"})                      .readValueScalar<float>();
+        this->_p->plcData.SPt100EntSec    = this->_p->client.getNode({2, "GVL.SPt100EntSec (%MD4)"})                      .readValueScalar<float>();
+        this->_p->plcData.SPt100SaiSec    = this->_p->client.getNode({2, "GVL.SPt100SaiSec (%MD5)"})                      .readValueScalar<float>();
+        this->_p->plcData.STpPoc1         = this->_p->client.getNode({2, "GVL.STpPoc1 (%MD6)"})                           .readValueScalar<float>();
+        this->_p->plcData.STpPoc2         = this->_p->client.getNode({2, "GVL.STpPoc2 (%MD7)"})                           .readValueScalar<float>();
+        this->_p->plcData.STpLen          = this->_p->client.getNode({2, "GVL.STpLen (%MD8)"})                            .readValueScalar<float>();
+        this->_p->plcData.SConPoc         = this->_p->client.getNode({2, "GVL.SConPoc (%MD9)"})                           .readValueScalar<float>();
+        this->_p->plcData.SConSaiPri      = this->_p->client.getNode({2, "GVL.SConSaiPri (%MD10)"})                       .readValueScalar<float>();
+        this->_p->plcData.STATE = 0;
     }
     catch (const std::exception& e)
     {
-        if (client.isConnected())
+        if (this->_p->client.isConnected())
         {
             std::cerr << stdErrorMsg("get_all()", "Cliente conectado, porém erro ao adquirir dados", e.what());
-            plcData.STATE = 1;
+            this->_p->plcData.STATE = 1;
         }
         else
         {
             std::cerr << stdErrorMsg("get_all()", "Cliente desconectado", e.what());
-            plcData.STATE = 2;
+            this->_p->plcData.STATE = 2;
         }
     }
 
-    return plcData;
+    return this->_p->plcData;
 }
 
